4ec9c3c80a21f83c1a405f7295464c635b5d9c0a
Demetrius Rapello <demetrius@corp.globo.com>
2011-07-18 10:20:00 -0300

    first commit

--- textAnalysis/views.py
+++ textAnalysis/views.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 from textAnalysis.statistics import extrair_tags
-from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 from globocore.common.solr import SolrConnection
 from django.conf import settings
 from common.materia_do_solr import MateriaDoSolr        
-
+import re
+from globocore.materia.models import Materia
 
 def classify(request):
     tags = ""
     textos = ""
-    materias = ""
+    materias = []
+    permalink = None
+    
+    if request.POST.get('permalink'):
+        permalink = request.POST.get('permalink')
+    
     
     if request.POST.get('titulo'):
         textos+=request.POST.get('titulo')
@@ -20,44 +27,40 @@ def classify(request):
     if request.POST.get('texto'):
         textos+=request.POST.get('texto')
         textos+=" "
+
     if textos:
+
         for (tag, f) in extrair_tags(textos):
             tags += "%s " % tag
         solr_connection = SolrConnection(settings.SOLRSERVER)
-        print tags    
+
         query = tags.encode('utf-8')
-        consulta = solr_connection.query(query + " isIssued:true", wt='json', start=0, rows=5,
+        consulta = solr_connection.query(query + " isIssued:true", wt='json', start=0, rows=15,
                                         indent='on', sort='issued', sort_order='desc')
         solr_connection.close()
-        for m in consulta.results:
-            materia = MateriaDoSolr(m)
-            # {% if noticia.thumbnail %} 
-            # <div class="foto">
-            #   <a class="borda-interna" href="{{ noticia.url }}"><img src="{{ noticia.thumbnail }}" /></a>
-            #   {% rounded_corners %}
-            # </div>
-            # {% endif %}
 
-            imagem = ""
-            link = ""
-            if materia.__dict__.has_key("url"):
-                link = materia.url
-
-            import re
-            from globocore.materia.models import Materia
-            from tectudo.templatetags.resize_image import resize
-            
+        for materiaSolr in consulta.results:
+            materia = MateriaDoSolr(materiaSolr)
             idMateria = re.search('(?<=[a-z]/)[0-9]*', materia.identifier).group(0)
             m = Materia.objects.get(id = idMateria)    
-            imagem = m.obtem_caminho_absoluto_de_delivery_do_thumbnail_para_plantao()
-            if imagem:
-                imagem = re.sub('http://anotherdomain.localhost:8000/po/tt/f/', 'http://s.glbimg.com/jo/g1/f/', imagem)
-                imagem = "<img style='width: 45px; height: 45px; padding-right:5px; float:left;' src='%s' />" % resize(imagem,"60x45")
-            else:
-                imagem = ""
             
-            title = materia.title
-            
-            materias += "<li style='float:left;'>%s <a href='%s'>%s</a></li>" % ( imagem, link, title)
-
-    return HttpResponse(materias)
+            # corta propria materia 
+            if permalink != m.permalink:
+                #gambi para mostrar foto em dev
+                imagem = m.obtem_caminho_absoluto_de_delivery_do_thumbnail_para_plantao()
+                if imagem:
+                    imagem = re.sub('http://anotherdomain.localhost:8000/po/tt/f/',
+                                    'http://s.glbimg.com/jo/g1/f/',
+                                    imagem)
+                    m.thumbnail = imagem
+                else:
+                    m.thumbnail = ""
+    
+                if m not in materias:
+                    materias += [m]
+    
+                if len(materias) >=5:
+                    break
+    
+    context = {'saibamais':materias}
+    return render_to_response('admin/materia/materia/materias.html', context, RequestContext(request,context) )
